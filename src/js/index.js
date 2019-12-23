7f76c86a7127a50f4147685175db736ae64fb1df
VictorUst <vityaust@yandex.ru>
2019-12-23 19:36:46 +0300

    Updates

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import '../scss/style.scss';
-import '../js/brandsView.js';
-import '../js/moreView.js';
-import '../js/typesTechView.js';
-import '../js/sidebarMenu.js';
-import '../js/modalCall.js';
-import '../js/modalFeel.js';
-import '../js/swiper-slider.js';
